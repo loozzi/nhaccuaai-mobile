78137544d7537fbeae62c9397a2f9607225fb926
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2025-05-03 11:51:51 +0700

    add react-native-track-player

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {LanguageProvider} from './src/contexts/languageContext';
 import {ThemeProvider} from './src/contexts/themeContext';
 import {NavigationContainer} from '@react-navigation/native';
@@ -9,10 +9,17 @@ import AuthNavigator from './src/navigation/auth';
 import {Provider as ReduxProvider} from 'react-redux';
 import {store} from './src/app/store';
 import StackPlayer from './src/components/layout/stack-player';
+import TrackPlayer from 'react-native-track-player';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  useEffect(() => {
+    TrackPlayer.setupPlayer().then(async () => {
+      // The player is ready to be used
+      console.log('TrackPlayer setup complete');
+    });
+  }, []);
   return (
     <ReduxProvider store={store}>
       <ThemeProvider>
